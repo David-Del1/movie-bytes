5231be4122c250f549a3cbc754176f531b82e33f
thankrandomness <vijaybchakilam@gmail.com>
2021-05-22 18:38:42 -0400

    jest global imports

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../src/App';
+
 describe('App', () => {
   it('Runs and passes', () => {
-    expect(true).toEqual(true);
+    render(<App />);
   });
 });
