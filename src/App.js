3b79705f66a703fa93297863c93d577a90476dba
David Delgadillo <daviddel.731@gmail.com>
2021-05-24 17:35:37 -0700

    did a whole lotta stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
-import Row from './Row';
-import requests from './request';
+import { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Home from '../home/Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import './App.css';
+import AuthPage from '../auth/AuthPage';
+import MoviesList from './movies-list/MoviesList';
 
-class App extends React.Component {
+class App extends Component {
+  state = {
+    token: window.localStorage.getItem('TOKEN')
+  }
+
+  handleUser = user => {
+    window.localStorage.setItem('TOKEN', user.token);
+    this.setState({ token: user.token });
+  }
+
   render() {
-    return (
-      <div className='App'>
-        <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
-        <Row title='Trending Tomorrow' fetchUrl={requests.fetchTrending} />
-        <Row title='Action' fetchUrl={requests.fetchActionMovies} />
-        <Row title='Comedy' fetchUrl={requests.fetchComedyMovies} />
-        <Row title='Horror' fetchUrl={requests.fetchHorrorMovies} />
-        <Row title='Romance' fetchUrl={requests.fetchRomanceMovies} />
-        <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
+    const { token } = this.state;
 
-        
+    return (
+      <div className="App">
+        <Router>
+          <Header/>
+          <main>
+
+            <Switch>
+              <Route path="/" exact={true}
+                render={routerProps => (
+                  <Home {...routerProps}/>
+                )}
+              />
+
+              <Route path="/auth" exact={true}
+                render={routerProps => (
+                  <AuthPage {...routerProps}
+                    onUser={this.handleUser}/>
+                )}
+              />
+
+              <Route path="/todopage"
+                render={routerProps => (
+                  token
+                    ? <ToDoPage {...routerProps}/>
+                    : <Redirect to="/auth"/>
+                )}
+              />
+
+              <Redirect to="/" />
+
+            </Switch>
+          </main>
+          <Footer/>
+        </Router>
       </div>
     );
   }
-}
+
+
 
 export default hot(module)(App);
