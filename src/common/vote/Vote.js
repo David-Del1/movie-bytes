501a5c9e6f3f6d0df523a64e1892f6aacab414a2
thankrandomness <vijaybchakilam@gmail.com>
2021-05-28 15:52:13 -0400

    fix setstate

--- src/common/vote/Vote.js
+++ src/common/vote/Vote.js
@@ -25,10 +25,9 @@ export default class Vote extends Component {
     try {
       const { movie, updateVoteCounts } = this.props;
       const { isUpVoted, isDownVoted } = this.state;
-      debugger;
       const values = await voteHandler(movie, isUpVoted, isDownVoted, clicked);
       debugger;
-      if (setState)
+      if (values.setState)
         this.setState({
           isUpVoted: values.isUpVoted,
           isDownVoted: values.isDownVoted,
